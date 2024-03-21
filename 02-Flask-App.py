22f92a5df8ebf23f87476d6eaaf15eb238df04c8
Karan Gautam <karan.g1997@outlook.com>
2024-03-21 21:30:23 +0530

    first commit

--- 02-Flask-App.py
+++ 02-Flask-App.py
@@ -1,4 +1,4 @@
-clsfrom flask import Flask, render_template, session, redirect, url_for, session
+from flask import Flask, render_template, session, redirect, url_for, session
 from flask_wtf import FlaskForm
 import pickle
 import joblib
